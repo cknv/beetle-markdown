b057f5c4f8f5372df689b0955fb846e57f9c70c9
Jeppe Toustrup <jeppe@tenzer.dk>
2014-08-23 10:35:45 +0200

    Added basic setup.py to allow easier package installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='beetle_markdown',
+    author='Esben Sonne',
+    author_email='esbensonne@gmail.com',
+    packages=[
+        'beetle_markdown'
+    ],
+    install_requires=[
+        'Markdown==2.4.1',
+    ],
+)
