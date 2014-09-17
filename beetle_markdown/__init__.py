f8d02ebe12f70f000ac023e1d5d8c65d97926936
Esben Sonne <esbensonne@gmail.com>
2014-09-17 16:04:27 +0200

    Updated to work with the new BeetleContext.

--- beetle_markdown/__init__.py
+++ beetle_markdown/__init__.py
@@ -1,9 +1,10 @@
 import markdown
-from beetle.context import content_renderer, writer
+
 
 def render_markdown(raw_content):
     return markdown.markdown(raw_content)
 
-def register(plugin_config, beetle_config):
+
+def register(ctx, plugin_config):
     markdown_extentions = ['md', 'mkd', 'markdown']
-    content_renderer.add_renderer(markdown_extentions, render_markdown)
+    ctx.content_renderer.add_renderer(markdown_extentions, render_markdown)
