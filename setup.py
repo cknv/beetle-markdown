0ff307890bb765488b08121dbde4ebf246ca95bd
Esben Sonne <esbensonne@gmail.com>
2014-09-17 16:01:14 +0200

    Changed email.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='beetle_markdown',
     author='Esben Sonne',
-    author_email='esbensonne@gmail.com',
+    author_email='esbensonne+code@gmail.com',
     url='https://github.com/cknv/beetle-markdown',
     license='MIT',
     packages=[
