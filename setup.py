61cf39c4ec866c80151364b20817a76be50e58c1
Esben Sonne <esbensonne@gmail.com>
2014-09-28 15:57:00 +0200

    Added version.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='beetle_markdown',
+    version='0.1.0'
     author='Esben Sonne',
     author_email='esbensonne+code@gmail.com',
     description='Markdown plugin for the static site generator Beetle',
