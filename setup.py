be4e808f9ce047df8c48b7fa6a940e040723c429
Esben Sonne <esbensonne@gmail.com>
2014-09-28 15:52:26 +0200

    Improved setup.py.

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ setup(
     name='beetle_markdown',
     author='Esben Sonne',
     author_email='esbensonne+code@gmail.com',
+    description='Markdown plugin for the static site generator Beetle',
     url='https://github.com/cknv/beetle-markdown',
     license='MIT',
     packages=[
         'beetle_markdown'
     ],
     install_requires=[
-        'Markdown==2.4.1',
+        'Markdown',
     ],
 )
